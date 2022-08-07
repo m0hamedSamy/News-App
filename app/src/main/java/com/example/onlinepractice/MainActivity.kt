255286d854d72f6c6050c08d414b950a227ab18e
Mohamed <mohamed0909samy@gmail.com>
2022-08-07 12:20:35 +0200

    The main fragment container created for a single activity architecture

--- app/src/main/java/com/example/onlinepractice/MainActivity.kt
+++ app/src/main/java/com/example/onlinepractice/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.onlinepractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.onlinepractice.login.LoginFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null){
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<LoginFragment>(R.id.main_fragment_container)
+            }
+        }
+
+
     }
+
+
+
 }
